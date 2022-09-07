94be832ca1cd2a11e2892a1a063e22730a1eccc2
ziss25 <ziss2517@gmail.com>
2022-09-07 14:11:19 +0800

    first commit

---
+++ build/main.js
@@ -0,0 +1,35 @@
+const newCard = document.querySelector('.card');
+
+function requestApi() {
+  let xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      let obj = JSON.parse(xhttp.response);
+      let data = obj.slip;
+      newCard.innerHTML = template(data);
+    }
+  };
+  xhttp.open('GET', 'https://api.adviceslip.com/advice', true);
+  xhttp.send();
+}
+
+function template(paramsData) {
+  let result = `
+  <p class="card__idText">ADVICE #${paramsData.id}</p>
+  <div class="card__quote">
+    <blockquote>${paramsData.advice}</blockquote>
+  </div>
+
+  <div class="pattern">
+    <img src="images/pattern-divider-mobile.svg" alt="" />
+  </div>
+
+  <div class="button" onclick="requestApi()">
+    <img src="images/icon-dice.svg" alt="" />
+  </div>
+  `;
+
+  return result;
+}
+
+requestApi();
---
+++ build/text.md
@@ -0,0 +1,18 @@
+<!-- Advice ID goes here -->
+
+      <p class="card__idText">ADVICE #2517</p>
+      <!-- Advice text goes here -->
+      <div class="card__quote">
+        <blockquote>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, minus.</blockquote>
+      </div>
+
+      <div class="pattern">
+        <img src="images/pattern-divider-mobile.svg" alt="" />
+      </div>
+
+      <div class="button">
+        <img src="images/icon-dice.svg" alt="" />
+      </div>
+      -->
+
+    */
